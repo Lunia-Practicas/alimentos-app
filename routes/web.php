20f85498e3a7385ef492e0ee03283cd660587e95
Jaime <jaimecaballero99@gmail.com>
2024-05-02 15:04:28 +0200

    Posible ruta para el ListAllProductsById

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\CreateProductController;
 use App\Http\Controllers\DeleteCategoryController;
 use App\Http\Controllers\DeleteProductController;
 use App\Http\Controllers\ListAllCategoriesController;
+use App\Http\Controllers\ListAllProductsByIdCategoryController;
 use App\Http\Controllers\ListAllProductsController;
 use App\Http\Controllers\UpdateCategoryController;
 use App\Http\Controllers\UpdateProductController;
@@ -19,6 +20,7 @@ Route::get('/categories', [ListAllCategoriesController::class, '__invoke'] );
 Route::post('/categories', [CreateCategoryController::class, '__invoke'] );
 Route::patch('/categories/{id}', [UpdateCategoryController::class, '__invoke'] );
 Route::delete('/categories/{id}', [DeleteCategoryController::class, '__invoke'] );
+Route::get('/categories/products/{id}', [ListAllProductsByIdCategoryController::class, '__invoke'] );
 
 Route::post('/products', [CreateProductController::class, '__invoke'] );
 Route::get('/products', [ListAllProductsController::class, '__invoke'] );
