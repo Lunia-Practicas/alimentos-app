66585737bed590de0fb7a568acd9d7ce7c190321
Jaime <jaimecaballero99@gmail.com>
2024-06-12 09:35:34 +0200

    Mensajes de error al enviar correo de solicitud de pedido

--- app/Listeners/SendOrderEmail.php
+++ app/Listeners/SendOrderEmail.php
@@ -5,6 +5,7 @@ namespace App\Listeners;
 use App\Events\OrderCreated;
 use App\Mail\OrderNotifyEmailAdmin;
 use App\Mail\OrderNotifyEmailClient;
+use App\Models\Audit;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Mail;
@@ -15,6 +16,7 @@ class SendOrderEmail implements ShouldQueue
 
     /**
      * Handle the event.
+     * @throws \ReflectionException
      */
     public function handle(OrderCreated $event)
     {
@@ -26,7 +28,62 @@ class SendOrderEmail implements ShouldQueue
         $note = $order->note;
         $email = $order->email;
 
-        Mail::to($email)->send(new OrderNotifyEmailClient($name, $total, $note, $quantity, $email));
-        Mail::to('jacain99laravel@gmail.com')->send(new OrderNotifyEmailAdmin($name, $total, $note, $quantity, $email));
+        $emailContent = (new OrderNotifyEmailClient($name, $total, $note, $quantity, $email))->render();
+
+        try {
+            Mail::to($email)->send(new OrderNotifyEmailClient($name, $total, $note, $quantity, $email));
+
+            $bodyStart = strpos($emailContent, '<body>') + 6; // Obtener el índice de inicio del cuerpo
+            $bodyEnd = strpos($emailContent, '</body>'); // Obtener el índice de fin del cuerpo
+            $bodyLength = $bodyEnd - $bodyStart; // Calcular la longitud del cuerpo
+            $bodyContent = substr($emailContent, $bodyStart, $bodyLength); // Extraer el contenido del cuerpo
+
+            Audit::create([
+                'addressee' => $email,
+                'subject' => 'Order Client Notification',
+                'body' => $bodyContent
+            ]);
+
+        }catch (\Exception $e){
+            $bodyStart = strpos($emailContent, '<body>') + 6; // Obtener el índice de inicio del cuerpo
+            $bodyEnd = strpos($emailContent, '</body>'); // Obtener el índice de fin del cuerpo
+            $bodyLength = $bodyEnd - $bodyStart; // Calcular la longitud del cuerpo
+            $bodyContent = substr($emailContent, $bodyStart, $bodyLength); // Extraer el contenido del cuerpo
+
+            Audit::create([
+                'addressee' => $email,
+                'subject' => 'Order Client Notification',
+                'body' => $bodyContent,
+                'error' => $e->getMessage()
+            ]);
+        }
+
+        try {
+            Mail::to("jacain99laravel@gmail.com")->send(new OrderNotifyEmailAdmin($name, $total, $note, $quantity, $email));
+
+            $bodyStart = strpos($emailContent, '<body>') + 6; // Obtener el índice de inicio del cuerpo
+            $bodyEnd = strpos($emailContent, '</body>'); // Obtener el índice de fin del cuerpo
+            $bodyLength = $bodyEnd - $bodyStart; // Calcular la longitud del cuerpo
+            $bodyContent = substr($emailContent, $bodyStart, $bodyLength); // Extraer el contenido del cuerpo
+
+            Audit::create([
+                'addressee' => "jacain99laravel@gmail.com",
+                'subject' => 'Order Client Notification',
+                'body' => $bodyContent
+            ]);
+        } catch (\Exception $e){
+            $bodyStart = strpos($emailContent, '<body>') + 6; // Obtener el índice de inicio del cuerpo
+            $bodyEnd = strpos($emailContent, '</body>'); // Obtener el índice de fin del cuerpo
+            $bodyLength = $bodyEnd - $bodyStart; // Calcular la longitud del cuerpo
+            $bodyContent = substr($emailContent, $bodyStart, $bodyLength); // Extraer el contenido del cuerpo
+
+            Audit::create([
+                'addressee' => "jacain99laravel@gmail.com",
+                'subject' => 'Order Client Notification',
+                'body' => $bodyContent,
+                'error' => $e->getMessage()
+            ]);
+        }
+
     }
 }
